92e82d66e25f453a398617bb8750417d2f24cf78
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2018-10-07 15:26:14 +0200

    Debian 9.5

--- debian/scripts/cleanup.sh
+++ debian/scripts/cleanup.sh
@@ -3,6 +3,19 @@
 # Store build time
 date > /etc/image_build_time
 
+DEBIAN_VERSION=$(lsb_release -sr)
+if [[ ${DEBIAN_VERSION} == 9.0 ]]; then
+    # When booting with Vagrant / VMware the PCI slot is changed from 33 to 32.
+    # Instead of eth0 the interface is now called ens33 to mach the PCI slot,
+    # so we need to change the networking scripts to enable the correct
+    # interface.
+    #
+    # NOTE: After the machine is rebooted Packer will not be able to reconnect
+    # (Vagrant will be able to) so make sure this is done in your final
+    # provisioner.
+    sed -i "s/ens33/ens32/g" /etc/network/interfaces
+fi
+
 # Make sure Udev doesn't block our network
 # http://6.ptmc.org/?p=164
 echo "cleaning up udev rules"
